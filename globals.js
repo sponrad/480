2d6daf59d60ed97f60edca19abcd950cf63c53be
Conrad <sponrad@gmail.com>
2016-02-25 12:00:46 -0800

    sorting / optimize

--- globals.js
+++ globals.js
@@ -1,6 +1,9 @@
-data = null;
-chart = null;
+redata = null;
+rechart = null;
+stockdata = null;
+stockchart = null;
 options = null;
 game = null;
 
+DELAY = 1000; //in ms
 INFLATION_RATE = 0.04;
--- init.js
+++ init.js
@@ -1,4 +1,9 @@
 game = new Game();
 
 google.charts.load('current', {packages: ['corechart', 'line']});
-google.charts.setOnLoadCallback(drawBackgroundColor);
+google.charts.setOnLoadCallback(drawCharts);
+
+
+rivets.bind($('#480'), { game: game });
+
+setInterval(update, DELAY);
